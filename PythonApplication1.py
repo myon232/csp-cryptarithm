3fa3c5ddd1669511e297999a9cea3be6fdbae059
myon232 <myon232@gmail.com>
2015-12-01 23:49:35 -0800

    Added a timer and some output.

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,24 +1,59 @@
-﻿from Ar import *
+﻿import timeit
+
+from Ar import *
 from Domain import*
-print ("we will run: send more money| please edit mylist in the python application to change")
+
+
+def printTimePass(stop, start):
+    s = stop - start
+    m = int(s / 60.0)
+    print("Time Elapsed: {:d}m {:.5}s".format(m, s))
+
+
+print ("we will run: send more money| please edit wordList in the python application to change")
 print("some puzzles may take longer")
 
-mylist = ["tiles","puzzles","picture"]
+wordList = ["tiles","puzzles","picture"]
+wordList = ["send","more","money"]
+
+# copy to a tmp list.
+tmpList = list(wordList)
+
+# CSP setup.
+var1 = extractSep(tmpList)[0] # extract char
+cList = setConstDict(tmpList)
+var4 = extractChars(tmpList)
+dMap = setDomainTable(extractChars(tmpList), tmpList)
 
 
-
-
-var1 = extractSep(mylist)[0]#extract char
-cList = setConstDict(mylist)
-var4 = extractChars(mylist)
-dMap = setDomainTable(extractChars(mylist),mylist)
+# Print the Domain.
+print("---Domain---")
 for name in dMap:
     print(dMap[name])
+print("------------\n")
 
 
+# Start the timer. 
+start = timeit.default_timer()
+# Run the CSP.
+isOkay = setWithMostConst(cList, dMap, tmpList)
+# Stop the timer.
+stop = timeit.default_timer()
 
-setWithMostConst(cList,dMap,mylist)
 
-print("tiles","puzzles","picture")
-for word in mylist:
-    print(wordToNum(word,dMap))
+# print some output. 
+if isOkay:
+    print("\nCSP Pass!!\n")
+    
+    for i in range(len(wordList)):
+    
+        if i == len(wordList) -1:
+            print("----------  ----------")
+    
+        print("{:>10} {:>11}".format(wordList[i], wordToNum(wordList[i], dMap)))
+    
+    print("\n")
+    printTimePass(stop, start)
+
+else:
+    print("CSP Fail!!")
